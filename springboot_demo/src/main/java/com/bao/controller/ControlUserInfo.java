85dd64187955f5b419a753caa04ea3fe5c994091
housohei <54211651+housohei@users.noreply.github.com>
2020-06-02 16:53:59 +0800

    first version

---
+++ springboot_demo/src/main/java/com/bao/controller/ControlUserInfo.java
@@ -0,0 +1,18 @@
+package com.bao.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@RestController
+public class ControlUserInfo {
+    @GetMapping(value = "/api/user/getUserName")
+    public String getUserName(HttpServletRequest request) throws ParseException {
+        String userName = "Joyi! The time is:" + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+        return userName;
+    }
+}
